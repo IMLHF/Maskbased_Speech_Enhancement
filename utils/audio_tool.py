18c49db12958f919c40b23d349bf940fcca84b76
IMLHF <red_wind@foxmail.com>
2019-02-14 21:43:17 +0800

    # tools

---
+++ utils/audio_tool.py
@@ -0,0 +1,30 @@
+import soundfile
+import numpy as np
+
+'''
+soundfile.info(file, verbose=False)
+soundfile.available_formats()
+soundfile.available_subtypes(format=None)
+soundfile.read(file, frames=-1, start=0, stop=None, dtype='float64', always_2d=False, fill_value=None, out=None, samplerate=None, channels=None, format=None, subtype=None, endian=None, closefd=True)
+soundfile.write(file, data, samplerate, subtype=None, endian=None, format=None, closefd=True)
+'''
+
+
+def write_audio(file, data, sr, bits, _format='wav'):
+  subtype = {
+      8: 'PCM_S8',
+      16: 'PCM_16',
+      24: 'PCM_24'
+  }[bits]
+  # -1.0 < data < 1.0, data.type=float
+  return soundfile.write(file, data, sr, subtype=subtype, format=_format)
+
+
+def read_audio(file, bits, _format='wav'):
+  subtype = {
+      8: 'PCM_S8',
+      16: 'PCM_16',
+      24: 'PCM_24'
+  }[bits]
+  data, sr = soundfile.read(file, subtype=subtype, format=_format)
+  return data, sr
