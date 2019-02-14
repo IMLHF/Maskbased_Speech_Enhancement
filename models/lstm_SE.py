3e07e3879c046c2baf4e45a384ed0320a7672842
IMLHF <red_wind@foxmail.com>
2019-02-14 21:43:35 +0800

    # training model

---
+++ models/lstm_SE.py
@@ -0,0 +1,184 @@
+# speech enhancement
+
+import tensorflow as tf
+import time
+import sys
+from tensorflow.contrib.rnn.python.ops import rnn
+from utils import tf_tool
+from FLAGS import PARAM
+from dataManager import mixed_aishell_tfrecord_io as data_tool
+
+
+
+class SE_MODEL(object):
+  def __init__(self,
+               x_mag_spec_batch,
+               y_mag_spec_batch,
+               lengths_batch,
+               theta_x_batch=None,
+               theta_y_batch=None,
+               infer=False):
+    self._x_mag_spec = x_mag_spec_batch
+    self._norm_x_mag_spec = norm_mag_spec(self._x_mag_spec)
+    self._norm_x_logmag_spec = norm_logmag_spec(self._x_mag_spec)
+
+    self._y_mag_spec = y_mag_spec_batch
+    self._norm_y_mag_spec = norm_mag_spec(self._y_mag_spec)
+    self._norm_y_logmag_spec = norm_logmag_spec(self._y_mag_spec)
+
+    self._lengths = lengths_batch
+
+    self.batch_size = tf.shape(self._lengths)[0]
+    self._model_type = PARAM.MODEL_TYPE
+
+    if PARAM.INPUT_TYPE == 'mag':
+      self._inputs = self._norm_x_mag_spec
+    elif PARAM.INPUT_TYPE == 'logmag':
+      self._inputs = self._norm_x_logmag_spec
+
+    if PARAM.LABEL_TYPE == 'mag':
+      self._labels = self._norm_y_mag_spec
+    elif PARAM.LABEL_TYPE == 'logmag':
+      self._labels = self._norm_y_logmag_spec
+
+    outputs = self._inputs
+
+    def lstm_cell():
+      return tf.contrib.rnn.LSTMCell(
+          PARAM.RNN_SIZE, forget_bias=1.0, use_peepholes=True,
+          num_proj=PARAM.LSTM_num_proj,
+          initializer=tf.contrib.layers.xavier_initializer(),
+          state_is_tuple=True, activation=PARAM.LSTM_ACTIVATION)
+    lstm_attn_cell = lstm_cell
+    if not infer and PARAM.KEEP_PROB < 1.0:
+      def lstm_attn_cell():
+        return tf.contrib.rnn.DropoutWrapper(lstm_cell(), output_keep_prob=PARAM.KEEP_PROB)
+
+    def GRU_cell():
+      return tf.contrib.rnn.GRUCell(
+          PARAM.RNN_SIZE,
+          # kernel_initializer=tf.contrib.layers.xavier_initializer(),
+          activation=PARAM.LSTM_ACTIVATION)
+    GRU_attn_cell = lstm_cell
+    if not infer and PARAM.KEEP_PROB < 1.0:
+      def GRU_attn_cell():
+        return tf.contrib.rnn.DropoutWrapper(GRU_cell(), output_keep_prob=PARAM.KEEP_PROB)
+
+    if PARAM.MODEL_TYPE.upper() == 'BLSTM':
+      with tf.variable_scope('BLSTM'):
+
+        lstm_fw_cell = tf.contrib.rnn.MultiRNNCell(
+            [lstm_attn_cell() for _ in range(PARAM.RNN_LAYER)], state_is_tuple=True)
+        lstm_bw_cell = tf.contrib.rnn.MultiRNNCell(
+            [lstm_attn_cell() for _ in range(PARAM.RNN_LAYER)], state_is_tuple=True)
+
+        lstm_fw_cell = lstm_fw_cell._cells
+        lstm_bw_cell = lstm_bw_cell._cells
+        result = rnn.stack_bidirectional_dynamic_rnn(
+            cells_fw=lstm_fw_cell,
+            cells_bw=lstm_bw_cell,
+            inputs=outputs,
+            dtype=tf.float32,
+            sequence_length=self._lengths)
+        outputs, fw_final_states, bw_final_states = result
+    if PARAM.MODEL_TYPE.upper() == 'BGRU':
+      with tf.variable_scope('BGRU'):
+
+        gru_fw_cell = tf.contrib.rnn.MultiRNNCell(
+            [GRU_attn_cell() for _ in range(PARAM.RNN_LAYER)], state_is_tuple=True)
+        gru_bw_cell = tf.contrib.rnn.MultiRNNCell(
+            [GRU_attn_cell() for _ in range(PARAM.RNN_LAYER)], state_is_tuple=True)
+
+        gru_fw_cell = gru_fw_cell._cells
+        gru_bw_cell = gru_bw_cell._cells
+        result = rnn.stack_bidirectional_dynamic_rnn(
+            cells_fw=gru_fw_cell,
+            cells_bw=gru_bw_cell,
+            inputs=outputs,
+            dtype=tf.float32,
+            sequence_length=self._lengths)
+        outputs, fw_final_states, bw_final_states = result
+
+    with tf.variable_scope('fullconnectOut'):
+      if self._model_type.upper()[0] == 'B':  # bidirection
+        outputs = tf.reshape(outputs, [-1, 2*PARAM.LSTM_num_proj])
+        in_size = 2*PARAM.LSTM_num_proj
+      out_size = PARAM.OUTPUT_SIZE
+      weights = tf.get_variable('weights1', [in_size, out_size],
+                                initializer=tf.random_normal_initializer(stddev=0.01))
+      biases = tf.get_variable('biases1', [out_size],
+                               initializer=tf.constant_initializer(0.0))
+      mask = tf.nn.relu(tf.matmul(outputs, weights) + biases)
+      self._mask = tf.reshape(
+          mask, [self.batch_size, -1, PARAM.OUTPUT_SIZE])
+
+    self.saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=30)
+    if infer:
+      if PARAM.DECODING_MASK_POSITION == 'mag':
+        self._cleaned = rm_norm_mag_spec(self._mask*self._norm_x_mag_spec)
+      elif PARAM.DECODING_MASK_POSITION == 'logmag':
+        self._cleaned = rm_norm_logmag_spec(self._mask*self._norm_x_logmag_spec)
+      return
+
+    if PARAM.TRAINING_MASK_POSITION == 'mag':
+      self._cleaned = self._mask*self._norm_x_mag_spec
+    elif PARAM.TRAINING_MASK_POSITION == 'logmag':
+      self._cleaned = self._mask*self._norm_x_logmag_spec
+    if PARAM.MASK_TYPE == 'PSIRM':
+      self._labels *= tf.cos(theta_x_batch-theta_y_batch)
+
+    if PARAM.TRAINING_MASK_POSITION != PARAM.LABEL_TYPE:
+      if PARAM.LABEL_TYPE == 'mag':
+        self._cleaned = logmag2mag(self._cleaned)
+      elif PARAM.LABEL_TYPE == 'logmag':
+        self._cleaned = mag2logmag(self._cleaned)
+    self._loss = PARAM.LOSS_FUNC(self._cleaned,self._labels)
+    if tf.get_variable_scope().reuse:
+      return
+
+    self._lr = tf.Variable(0.0, trainable=False)
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars),
+                                      PARAM.CLIP_NORM)
+    optimizer = tf.train.AdamOptimizer(self.lr)
+    #optimizer = tf.train.GradientDescentOptimizer(self.lr)
+    self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+    self._new_lr = tf.placeholder(
+        tf.float32, shape=[], name='new_learning_rate')
+    self._lr_update = tf.assign(self._lr, self._new_lr)
+
+  def assign_lr(self, session, lr_value):
+    session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
+
+  @property
+  def cleaned(self):
+    return self._cleaned
+
+  @property
+  def inputs(self):
+    return self._inputs
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def mask(self):
+    return self._activations
+
+  @property
+  def lengths(self):
+    return self._lengths
+
+  @property
+  def lr(self):
+    return self._lr
+
+  @property
+  def loss(self):
+    return self._loss
+
+  @property
+  def train_op(self):
+    return self._train_op
